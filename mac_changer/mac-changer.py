d6b11caf715c1ceedce22240666cde90c2e4565a
root <root@localhost.localdomain>
2018-09-24 12:47:21 -0400

    add error message

--- mac_changer/mac-changer.py
+++ mac_changer/mac-changer.py
@@ -5,26 +5,39 @@ import optparse  # required for getting user input from args
 
 
 def get_arguments():
-    parser = optparse.OptionParser()
-    parser.add_option("-s", "--shell", dest="shell",
+    """
+    Run and input the following:
+    python mac-changer.py -s ifconfig -i eth0 -m 00:11:22:33:44:55
+    then return the argument values
+    """
+    p = optparse.OptionParser()
+    p.add_option("-s", "--shell", dest="shell",
             help="shell command for ip address (eg. ifconfig)")
-    # give -i or --interface option, store in dest, give help if user requires
-    parser.add_option("-i", "--interface", dest="interface",
+    p.add_option("-i", "--interface", dest="interface",
             help="Interface to change MAC address")
-    parser.add_option("-m", "--mac", dest="new_mac",
+    p.add_option("-m", "--mac", dest="new_mac",
             help="New MAC address")
-    return parser.parse_args()  # return options & arguments  value
+    (_options, _arguments) = p.parse_args()
+    if not _options.shell:
+        # code to handle error
+        p.error("[-] Please specify a shell command, use --help for more info")
+    elif not _options.interface:
+        p.error("[-] Please specify an interface, use --help for more info")
+    elif not _options.new_mac:
+        # code to handle error
+        p.error ("[-] Please specify a new MAC address, use --help for more info")
+    return _options
 
 
 def change_mac(shell, interface, new_mac):
-    # disable ethernet_interface if it exist
+    """
+    Disable interface -> change MAC address -> enable interface
+    """
     subprocess.call([shell, interface, "down"])
-    # change ethernet_interface's mac address
     subprocess.call([shell, interface, "hw", "ether", new_mac])
-    # enable ethernet_interface
     subprocess.call([shell, interface, "up"])
     print(f"MAC address has been changed to {new_mac} for {interface}")
 
 
-(options, arguments) = get_arguments()
+options = get_arguments()
 change_mac(options.shell, options.interface, options.new_mac)
