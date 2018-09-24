5f132af411449113a75e09b932ca6c82204366fd
root <root@localhost.localdomain>
2018-09-24 11:26:49 -0400

    change subprocess call process for more user interface security

--- mac_changer/mac-changer.py
+++ mac_changer/mac-changer.py
@@ -2,18 +2,19 @@
 
 import subprocess  # required to run shell commands
 
+
 ip_command = input("ip address shell command: ")
 interface = input("ethernet interface (eg. eth0): ")
 new_mac = input("new MAC (12 digits eg.00:11:22:33:44:55): ")
 
 print(f"+ Changing MAC address for {interface} to {new_mac}")
 # disable ethernet_interface if it exist
-subprocess.call(f"{ip_command} {interface} down", shell=True)  # not safe
+subprocess.call([ip_command, interface, "down"])  # all commands run after index 0 is a subcommand
 print(f"{interface} has been disabled")
 # change ethernet_interface's mac address
-subprocess.call(f"{ip_command} {interface} hw ether {new_mac}", shell=True)
+subprocess.call([ip_command, interface, "hw", "ether", new_mac])
 print(f"mac address has been changed to {new_mac}")
 # enable ethernet_interface
-subprocess.call("{ip_command} {interface} up", shell=True)
+subprocess.call([ip_command, interface, "up"])
 print(f"{interface} has been enabled")
 
