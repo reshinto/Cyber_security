7570d25261093b390c77cc10677956f19c07e080
root <root@localhost.localdomain>
2018-09-24 11:24:41 -0400

    add user interface

--- mac_changer/mac-changer.py
+++ mac_changer/mac-changer.py
@@ -2,12 +2,18 @@
 
 import subprocess  # required to run shell commands
 
+ip_command = input("ip address shell command: ")
+interface = input("ethernet interface (eg. eth0): ")
+new_mac = input("new MAC (12 digits eg.00:11:22:33:44:55): ")
 
+print(f"+ Changing MAC address for {interface} to {new_mac}")
 # disable ethernet_interface if it exist
-subprocess.call("ifconfig eth0 down", shell=True)
+subprocess.call(f"{ip_command} {interface} down", shell=True)  # not safe
+print(f"{interface} has been disabled")
 # change ethernet_interface's mac address
-subprocess.call("ifconfig eth0 hw ether 00:11:22:33:44:55", shell=True)
+subprocess.call(f"{ip_command} {interface} hw ether {new_mac}", shell=True)
+print(f"mac address has been changed to {new_mac}")
 # enable ethernet_interface
-subprocess.call("ifconfig eth0 up, shell=True)
-
+subprocess.call("{ip_command} {interface} up", shell=True)
+print(f"{interface} has been enabled")
 
