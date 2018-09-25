dc6cc5357a2aa84c9380906f520a7ca396445a66
reshinto <terencekong2002@gmail.com>
2018-09-26 01:09:01 +0900

    change optparse to argparse module

--- mac_changer/mac_spoof.py
+++ mac_changer/mac_spoof.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
-
+"""
+MAC address spoofer. Currently works in Kali linux, may work for Ubuntu.
+Code works for Mac OSX, but not functioning as it should.
+TODO: implement windows ipconfig command
+"""
 import subprocess  # required to run shell commands
-import optparse  # required for getting user input from args
+import argparse  # required for getting user input from args
 import re
 
 
@@ -11,45 +15,46 @@ def get_arguments():
     python mac-changer.py -s ifconfig -i eth0 -m 00:11:22:33:44:55
     then return the argument values
     """
-    p = optparse.OptionParser()
-    p.add_option("-s", "--shell", dest="shell",
-                 help="shell command for ip address (eg. ifconfig)")
-    p.add_option("-i", "--interface", dest="interface",
-                 help="Interface to change MAC address")
-    p.add_option("-m", "--mac", dest="new_mac",
-                 help="New MAC address")
-    (_options, _arguments) = p.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--shell", dest="shell",
+                        help="shell command for ip address (eg. ifconfig)")
+    parser.add_argument("-i", "--interface", dest="interface",
+                        help="Interface to change MAC address")
+    parser.add_argument("-m", "--mac", dest="new_mac",
+                        help="New MAC address")
+    (_options, _arguments) = parser.parse_args()
     if not _options.shell:
-        p.error(
+        parser.error(
             "[-] Please specify a shell command, use --help for more info")
     elif not _options.interface:
-        p.error("[-] Please specify an interface, use --help for more info")
+        parser.error("[-] Please specify an interface, "
+                     "use --help for more info")
     elif not _options.new_mac:
-        p.error(
-            "[-] Please specify a new MAC address, use --help for more info")
+        parser.error("[-] Please specify a new MAC address, "
+                     "use --help for more info")
     return _options
 
 
-def change_mac(shell, interface, new_mac, hw=None):
+def change_mac(shell, interface, new_mac, kali=False):
     """
     shell = ifconfig
     Disable interface -> change MAC address -> enable interface
     """
     print(f"[+] Changing MAC address for {interface}")
     subprocess.call([shell, interface, "down"])
-    _change_mac(shell, interface, new_mac, hw)
+    _change_mac(shell, interface, new_mac, kali)
     subprocess.call([shell, interface, "up"])
 
 
-def _change_mac(shell, interface, new_mac, hw=None):
+def _change_mac(shell, interface, new_mac, kali=False):
     """
     Check if hw is required
     hw is required for Kali linux
     """
-    if hw is None:
+    if kali is False:
         subprocess.call([shell, interface, "ether", new_mac])
     else:
-        subprocess.call([shell, interface, hw, "ether", new_mac])
+        subprocess.call([shell, interface, "hw", "ether", new_mac])
 
 
 def new_change_mac(shell, interface, new_mac):
