04c0f6c56f3ab89c4edd7e3eb1e74ea9ee755d27
reshinto <terencekong2002@gmail.com>
2018-09-26 22:48:19 +0900

    modify to add sending & receiving packets

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -5,7 +5,11 @@ import scapy.all as scapy
 
 
 def scan(ip):
-    scapy.arping(ip)
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1)
+    print(answered.summary())
 
 
 scan("10.0.2.1/24")
