55a0aae711db91a9df56b21730f998ffe1012bd6
reshinto <terence_kong2002@hotmail.com>
2018-09-29 16:53:10 -0400

    rewrite everything, add basic ARP function

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -5,11 +5,15 @@ import scapy.all as scapy
 
 
 def scan(ip):
-    arp_request = scapy.ARP(pdst=ip)
-    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast/arp_request
-    answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1)
-    print(answered.summary())
+    """
+    Scans the ip address input to find the MAC address of the respective
+    ip address by using the Address Resolution Protocol (arp) method:
+    scapy.arping(xxx)
+    """
+    scapy.arping(ip)
 
 
+# 1/24 = range from 0 to 254
+# this will allow you to scan all ip address
+# real ip, use e.g.: xxx.xxx.1.1/24
 scan("10.0.2.1/24")
