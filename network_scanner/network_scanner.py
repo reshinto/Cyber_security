e7e7b5e688fbe3fb9892f0f9c7d9b972dcbeb495
reshinto <terence_kong2002@hotmail.com>
2018-09-30 07:18:45 -0400

    split code into reusable functions

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -6,28 +6,21 @@ import scapy.all as scapy
 
 def scan(ip):
     """
-    Scans the ip address input to find the MAC address of the respective
-    ip address by using the Address Resolution Protocol (arp) method:
-    scapy.arping(xxx)
+    Scans the ip address input to find the MAC address
     """
-    # writing our own arp method to discover clients on network
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
 
-    # send packet & receive response
-    """send & receive function
-    there is also a scapy.sr()
-    difference between srp and sr is, srp allows us to send packets with
-    a custom Ether packet
-    add timeout you quit if get no response within 1 sec
-    """
     # use verbose to get rid of header text
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    answered_list, unanswered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)
+    return answered_list
 
+
+def show(scanned_list_result):
     print("IP\t\t|\tMAC Address\n" + "-"*50)
     # parsing response
-    for i in answered_list:
+    for i in scanned_list_result:
         # psrc to display IP address, hwsrc to display MAC address
         print(f"{i[1].psrc}\t|\t{i[1].hwsrc}\n" + "-"*50)
 
@@ -35,4 +28,5 @@ def scan(ip):
 # 1/24 = range from 0 to 254
 # this will allow you to scan all ip address
 # real ip, use e.g.: xxx.xxx.1.1/24
-scan("10.0.2.1/24")
+result = scan("10.0.2.1/24")
+show(result)
